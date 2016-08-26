dcf20b2441603277785e6522cd607754c183f73c
Sarah Dabaja <swroblewski5@live.com>
2016-08-26 11:56:57 -0400

    first commit

---
+++ src/Car.java
@@ -0,0 +1,36 @@
+
+public class Car {
+
+	String make;
+	public Car(String make, String model, int year, double price) {
+	
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	String model;
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	int year;
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	double price;
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
---
+++ src/UsedCar.java
@@ -0,0 +1,24 @@
+
+public class UsedCar extends Car {
+	
+	int addMiles;
+	
+	public static UsedCar(String make, String model, int year, double price, int miles) {
+		
+	}
+	public String toString(){
+		return addMiles;
+		
+	}
+	
+
+	public int getAddMiles() {
+		return addMiles;
+	}
+
+	public void setAddMiles(int addMiles) {
+		this.addMiles = addMiles;
+	}
+	}
+	
+
